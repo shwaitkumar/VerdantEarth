16cb3d51f018fc86fc2855fe713c8e9d3fe894c1
shwaitkumar <shwaitkumar@icloud.com>
2022-01-28 18:13:16 +0530

    added image picker

--- VerdantEarth/View Controllers/Main/ProfileViewController.swift
+++ VerdantEarth/View Controllers/Main/ProfileViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileViewController: BaseUiViewController {
+class ProfileViewController: BaseUiViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var viewProfilePicBg: UIView!
     
@@ -19,6 +19,11 @@ class ProfileViewController: BaseUiViewController {
     @IBOutlet weak var btnDone: UIButton!
     
     var picAnimated = false
+    
+    var imagePicker:UIImagePickerController?
+    private weak var delegate: UIImagePickerControllerDelegate?
+    
+    var userBase64 = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +40,12 @@ class ProfileViewController: BaseUiViewController {
         
         btnDone.layer.cornerRadius = 10
         
+        let tapProfilePicGesture = UITapGestureRecognizer(target: self, action:  #selector(self.addOrUpdateProfilePic))
+        self.viewProfilePicBg.addGestureRecognizer(tapProfilePicGesture)
+        
+        self.imagePicker = UIImagePickerController()
+        self.imagePicker?.delegate = self
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,6 +56,10 @@ class ProfileViewController: BaseUiViewController {
             animateProfilePic()
         }
 
+    }
+    
+    @objc func addOrUpdateProfilePic(sender : UITapGestureRecognizer) {
+        openCameraGallery()
     }
     
     func animateProfilePic() {
@@ -62,6 +77,88 @@ class ProfileViewController: BaseUiViewController {
     }
     
     @IBAction func btnDone(_ sender: Any) {
+    }
+    
+    func openCameraGallery()
+    {
+
+        let alert = UIAlertController(title: "Verdant Earth", message: "Select Option", preferredStyle: .actionSheet)
+            
+        alert.addAction(UIAlertAction(title: "Camera", style: .default , handler:{ (UIAlertAction)in
+                self.present(self.imagePicker!, animated: true, completion: {
+                    self.imagePicker?.sourceType = .camera
+                    //self.imagePicker?.allowsEditing = true
+                    self.imagePicker?.delegate = self
+                })
+            }))
+            
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default , handler:{ (UIAlertAction)in
+                
+                self.present(self.imagePicker!, animated: true, completion: {
+                    self.imagePicker?.sourceType = .photoLibrary
+                    //self.imagePicker?.allowsEditing = true
+                    self.imagePicker?.delegate = self
+                })
+            }))
+            
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction)in
+                print("User click Dismiss button")
+            }))
+            
+            self.present(alert, animated: true, completion: {
+                print("completion block")
+        })
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        {
+            ivUser.image = editedImage.fixedOrientation()
+        }
+        else  if let editedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            ivUser.image = editedImage.fixedOrientation()
+        }
+        
+        //Dismiss the UIImagePicker after selection
+        picker.dismiss(animated: true, completion: nil)
+        
+        getImageBase64()
+        
+    }
+    
+    //MARK: Image picker delegate methods
+    func doneButtonDidPress(_ imagePicker: UIImagePickerController, images: [UIImage]) {
+        
+        ivUser.image = images[0].fixedOrientation()
+        
+        getImageBase64()
+        
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: UIImagePickerController) {
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func wrapperDidPress(_ imagePicker: UIImagePickerController, images: [UIImage]) {
+        
+        ivUser.image = images[0].fixedOrientation()
+        
+        getImageBase64()
+        
+    }
+    
+    func getImageBase64()
+    {
+        
+        if(ivUser.image != nil)
+        {
+            let imageData:NSData = ivUser.image!.jpegData(compressionQuality:  0.3)! as NSData
+            userBase64 = imageData.base64EncodedString(options: .lineLength64Characters)
+        }
+        
     }
     
 }
